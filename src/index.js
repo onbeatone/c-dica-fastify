14c5b8616b6b70a673c7e9c570d601369a903653
Ocus Corporation <onbeatone@gmail.com>
2026-06-19 11:08:55 -0500

    pug layouts commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import fastify from 'fastify';
+import view from '@fastify/view';
+import pug from 'pug';
 
 const app = fastify();
 const port = 3000;
 
+await app.register(view, { engine: { pug } } );
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.view('src/views/index', { title: 'Home' });
 });
 
 app.get('/hello', (req, res) => {
@@ -15,6 +19,41 @@ app.get('/hello', (req, res) => {
 app.get('/users/:id/post/:postId', (req, res) => {
   const { id, postId } = req.params;
   res.send(`User ID: ${id}, Post ID: ${postId}`);
+});
+
+const state = {
+  courses: [
+    {
+      id: 1,
+      titulo: 'JS: Arrays',
+      descripcion: 'Curso sobre arrays en JavaScript',
+    },
+    {
+      id: 2,
+      titulo: 'JS: Funciones',
+      descripcion: 'Curso sobre funciones en JavaScript',
+    },
+  ],
+};
+
+app.get('/courses/:id', (req, res) => {
+  const { id } = req.params
+  const course = state.courses.find(({ id: courseId }) => courseId === parseInt(id));
+  if (!course) {
+    res.code(404).send({ message: 'Course not found' });
+    return;
+  }
+  const data = {
+    course,
+  };
+  res.view('src/views/courses/show', data);
+});
+
+app.get('/courses', (req, res) => {
+  const data = {
+    courses: state.courses,
+  };
+  res.view('src/views/courses/index', data);
 });
 
 app.listen({ port }, () => {
